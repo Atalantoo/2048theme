d2e37d53e984b4564a39b3ea7c21fd0b6a5b0e04
Damien FREMONT <damien.fremont@gmail.com>
2017-07-24 09:26:45 +0200

    test: framework

---
+++ app-cars/Test/CommonsTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace Test
+{
+    class CommonsTest
+    {
+        public static string[][] loadInput(string path)
+        {
+            string[] lines = File.ReadAllLines(path);
+            string[][] res = new string[lines.Length][];
+            for (int i = 0; i < lines.Length; i++)
+            {
+                string line = lines[i];
+                string[] words = line.Split(' ');
+
+                res[i] = new string[words.Length];
+                for (int j = 0; j < words.Length; j++)
+                {
+                    res[i][j] = words[j];
+                }
+            }
+            return res;
+        }
+    }
+}
---
+++ app-cars/Test/CommonsTestTest.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test
+{
+    [TestClass]
+    public class CommonsTestTest
+    {
+        [TestMethod]
+        public void loadInput()
+        {
+            string[][] res = CommonsTest.loadInput("../../test_case-00-input.txt");
+            Assert.IsNotNull(res);
+            Assert.AreEqual(5, res.Length);
+        }
+    }
+}
