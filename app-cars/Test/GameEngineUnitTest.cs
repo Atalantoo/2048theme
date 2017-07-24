6ed24535767b4cda53d63f42c2991d0916ac0342
Damien FREMONT <damien.fremont@gmail.com>
2017-07-24 09:26:35 +0200

    engine: test project

---
+++ app-cars/Test/GameEngineUnitTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test
+{
+    [TestClass]
+    public class GameEngineUnitTest
+    {
+        [TestMethod]
+        public void usecase_01()
+        {
+            Assert.IsTrue(true);
+        }
+
+    }
+}
