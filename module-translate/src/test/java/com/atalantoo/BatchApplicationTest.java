e1eea5c3b0ae88528e0602b2a5dcf3061130b739
Damien FREMONT <damien.fremont@gmail.com>
2017-09-04 22:04:11 +0200

    lang
    store

--- module-translate/src/test/java/com/atalantoo/BatchApplicationTest.java
+++ module-translate/src/test/java/com/atalantoo/BatchApplicationTest.java
@@ -22,38 +22,52 @@ public class BatchApplicationTest {
 	public OutputCapture outputCapture = new OutputCapture();
 
 	@Parameter
-	public String src;
+	public String type;
 	@Parameter(1)
-	public String dest;
+	public String src;
 	@Parameter(2)
-	public String src_lang;
+	public String dest;
 	@Parameter(3)
+	public String src_lang;
+	@Parameter(4)
 	public String dest_lang;
 
-	@Parameters( name = "project-2048 {index}: {3}")
+	@Parameters( name = "project-2048 {0} {index}: {4}")
 	public static Collection<Object[]> data() {
 		String path = System.getProperty("user.dir");
 		String source = "en";
-		String file = path + "\\..\\Project 2048 Cars\\Assets\\Project 2048\\Resources\\i18n\\locale-%s.json";
 		String[] targets = new String[] { //
+				"fr", //
 				"ja", //
 				"ko", //
 				"zh-TW", //
 				"de", //
-				"fr", //
 				"pt", //
 				"es", //
 				"it", //
 				"ru" //
-		};
-		String[][] parameters = new String[targets.length][4];
+		};		
+		String[][] parameters = new String[targets.length*2][5];
+
+		String file = path + "\\..\\Project 2048 Cars\\AppStore\\locale-%s.json";
 		for (int i = 0; i < targets.length; i++) {
 			String target = targets[i];
-			parameters[i][0] = format(file, source);
-			parameters[i][1] = format(file, target);
-			parameters[i][2] = source;
-			parameters[i][3] = target;
+			parameters[i][0] = "store";
+			parameters[i][1] = format(file, source);
+			parameters[i][2] = format(file, target);
+			parameters[i][3] = source;
+			parameters[i][4] = target;
 		}
+		file = path + "\\..\\Project 2048 Cars\\Assets\\Project 2048\\Resources\\i18n\\locale-%s.json";
+		for (int i = 0; i < targets.length; i++) {
+			String target = targets[i];
+			parameters[i+targets.length-1][0] = "app";
+			parameters[i+targets.length-1][1] = format(file, source);
+			parameters[i+targets.length-1][2] = format(file, target);
+			parameters[i+targets.length-1][3] = source;
+			parameters[i+targets.length-1][4] = target;
+		}
+		
 		return Arrays.asList(parameters);
 	}
 
