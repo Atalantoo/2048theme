620247861d5bc32f127088c48b6ac56fad2ae774
Damien FREMONT <damien.fremont@gmail.com>
2017-09-08 17:08:48 +0200

    navbar
    app subname

---
+++ Project 2048 Cars/Assets/Project 2048/Scripts/Components/NavBar.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+using Commons;
+using SimpleJSON;
+
+class NavBar : MonoBehaviour
+{
+    public string Name ="";
+    GameObject AppSubnameText;
+
+    void Start()
+    {
+        string subname = Name.ToUpper() + "!";
+        AppSubnameText = gameObject.FindChild("AppSubnameText");
+        AppSubnameText.GetComponent<Text>().text = subname;
+    }
+}
