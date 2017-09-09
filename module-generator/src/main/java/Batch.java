810def64760df0d95618d6d403494c2d00b20cf3
Damien FREMONT <damien.fremont@gmail.com>
2017-09-09 12:46:37 +0200

    refactoring

--- module-generator/src/main/java/Batch.java
+++
@@ -1,9 +0,0 @@
-import org.springframework.boot.SpringApplication;
-
-public class Batch {
-
-	public static void main(String[] args) {
-		System.exit(SpringApplication.exit(SpringApplication.run( //
-				BatchConfig.class, args)));
-	}
-}
